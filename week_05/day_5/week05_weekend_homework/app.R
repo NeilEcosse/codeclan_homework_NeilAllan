5772c9e7658ef17c424c231d697fa1c259344b23
Neil Allan <neilneeds@yahoo.co.uk>
2021-01-10 18:54:13 +0000

    week 5 weekend homework complete

--- week_05/day_5/week05_weekend_homework/app.R
+++ week_05/day_5/week05_weekend_homework/app.R
@@ -4,7 +4,23 @@ library(CodeClanData)
 library(sparkline)
 library(shinythemes)
 library(shinydashboard)
-library(DT)
+library(scales)
+
+# This app is just going to let you select a publisher
+# and a date range, and then show a bar chart of sales
+# over time, along with a "top 5" of best selling and 
+#  best rated games
+
+# I don't think this is the most useful view of the data -
+# e.g. wouldn't let you compare to a competitor, and I'm not
+# sure that selecting a smaller/larger date range adds anything
+# to your understanding of the data. Just practicing at the moment!
+
+# I've left in comments I was using after closed brackets
+# which explain what they do - I'm still getting easily confused
+# by these
+
+
 
 games_list <- unique(sort(game_sales$name))
 developer_list <- unique(sort(game_sales$developer))
@@ -13,7 +29,7 @@ publisher_list <- unique(sort(game_sales$publisher))
 year_list <- unique(sort(game_sales$year_of_release))
 
 ui <- fluidPage(
-    theme = shinytheme("cerulean"),
+    theme = shinytheme("paper"),
     
     titlePanel("Game Sales"),
     
@@ -24,7 +40,7 @@ ui <- fluidPage(
         column(4, 
                
                selectInput("publisher_select",
-                            "Select publisher",
+                            tags$h5("Select publisher"),
                             choices = publisher_list
                )
                
@@ -34,12 +50,12 @@ ui <- fluidPage(
         column(8,
                
                sliderInput("year_select",
-                              "Select date range",
+                              tags$h5("Select date range"),
                               min = min(game_sales$year_of_release),
                               max = max(game_sales$year_of_release),
                               value = c(min(game_sales$year_of_release), max(game_sales$year_of_release)),
                               sep ="",
-                              width = '100%'
+                              width = '95%'
                               
                )
         ), # close 2nd column      
@@ -62,14 +78,35 @@ ui <- fluidPage(
     fluidRow(
         
        column(4, 
-              titlePanel("Top 5 selling games in this period"),
+              titlePanel(tags$h5("Top selling games")),
+              titlePanel(tags$h6("Total for all platforms")),
                tableOutput("biggest_selling_titles")
                
         ), #close 1st column
+       
+       column(4, 
+              titlePanel(tags$h5("Top critic scores")),
+              titlePanel(tags$h6("Average across all platforms")),
+              tableOutput("best_critic_rating")
+              
+       ), #close 2nd column
+       
+       column(4, 
+              titlePanel(tags$h5("Top user scores")),
+              titlePanel(tags$h6("Average across all platforms")),
+              tableOutput("best_user_rating")
+              
+       ), #close 2nd column
     ), # close fluidrow 3     
    
     
 ) #close fluidpage
+
+
+
+
+# I think the server section could be more efficient, not sure 
+# what to change at the moment, though
 
 server <- function(input, output) {
     
@@ -82,8 +119,10 @@ server <- function(input, output) {
             filter(publisher == input$publisher_select) %>%
             filter(year_of_release %in% years_from_slider()) %>% 
             ggplot() +
-            aes(x = year_of_release, y = sales) +
+            aes(x = year_of_release, y = sales,) +
             geom_col() +
+            theme_minimal() +
+        scale_x_continuous(labels = number_format(accuracy = 1)) +
             labs(
                 title =  "Sales by year",
                 x = "Year",
@@ -91,7 +130,7 @@ server <- function(input, output) {
             ) 
     }) #end 1st
    # get  boxes showing  highest selling games and highest rated games
-    # Not sure how to do this
+    
     output$biggest_selling_titles <- renderTable({
     
         game_sales %>%
@@ -103,9 +142,34 @@ server <- function(input, output) {
             head(5)
         
   
-    })# end 2nd
-}
+    }, width = "90%")# end 2nd
 
+output$best_critic_rating <- renderTable({
+    
+    game_sales %>%
+        filter(publisher == input$publisher_select) %>%
+        filter(year_of_release %in% years_from_slider()) %>% 
+        group_by(name) %>% 
+        summarise(`avg critic rating` = mean(critic_score)) %>%
+        arrange(desc(`avg critic rating`)) %>% 
+        head(5)
+    
+    
+}, width = "90%")# end 3rd
+
+output$best_user_rating <- renderTable({
+    
+    game_sales %>%
+        filter(publisher == input$publisher_select) %>%
+        filter(year_of_release %in% years_from_slider()) %>% 
+        group_by(name) %>% 
+        summarise(`avg user rating` = mean(user_score)) %>%
+        arrange(desc(`avg user rating`)) %>% 
+        head(5)
+    
+    
+}, width = "90%")# end 4th
+}
 shinyApp(ui = ui, server = server)
 
 
